67a16afdfb2fcfed445c55e6007cb6771ce777e1
Michael Krämer <info.m.kraemer@gmail.com>
2024-09-03 18:06:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule , RouterOutlet, HeaderComponent, FruitlistComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+
+// @Input()variabel kann ich nutzen, um einen Wert von einem Child Component an ein Parent Component zu geben.
+// Der Wert innerhalb in Input kann (muss aber nicht) definiert werden. 
+//Wenn nicht, wird einfach die Variable nach der klammer im HTML verwendet.
+// Wenn sie verwendet wird, muss sie allerdings auch so im HTML verwendet werden. 
+// Beispiel: <app-singlefruit [fruitNumber]="index"></app-singlefruit>
+
+export class SinglefruitComponent {
+
+  starsCount = [1, 2, 3, 4, 5];
+
+
+  @Input()fruit = 
+
+  {
+    name: "Apfel",
+    img:"apple.png",
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 0,
+    reviews:[{name: "Waldemar W.", text: "gut für Obstsalat"},{name: "Olaf P.", text: "Kann man mal machen"}],
+  }; 
+
+
+  @Output()fruitName = new EventEmitter<string>();
+
+  emitName(){
+    this.fruitName.emit(this.fruit.name);
+  }
+
+}
